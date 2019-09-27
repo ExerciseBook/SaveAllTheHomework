40ad0bca123ad3d58a9ffea2eda82e83183077ae
Eric_Lian <git@SuperExerciseBook.com>
2019-09-28 01:03:42 +0800

    Get the information from emails.

--- SaveAllTheHomework/Source/HomeworkBot.cs
+++ SaveAllTheHomework/Source/HomeworkBot.cs
@@ -9,20 +9,18 @@ using Office = Microsoft.Office.Core;
 using System.Collections;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
-//using Microsoft.Toolkit.Uwp.Notifications;
-//using Windows.UI.Notifications;
-//using SaveAllTheHomework.Source.DesktopNotifications;
-//using Windows.Data.Xml.Dom;
 
 namespace SaveAllTheHomework.Source
 {
     public class HomeworkBot
     {
-        public HomeworkBot() {
-            
+        public HomeworkBot()
+        {
+
         }
 
-        public int SaveAllHomework() {
+        public int SaveAllHomework()
+        {
 
 
 
@@ -31,15 +29,18 @@ namespace SaveAllTheHomework.Source
             List<HomeworkItem> HomeworkList = new List<HomeworkItem>();
 
 
-            foreach (dynamic myItem in ActivedFolder.Items) {
+            foreach (dynamic myItem in ActivedFolder.Items)
+            {
                 /// myItem.Sender.Address System.String
                 /// myItem.Attachments.Count
                 /// 
                 /// myItem.CreationTime System.DateTime
                 /// myItem.ConversationTopic System.String
+                /// 		SenderEmailAddress	"***REMOVED***"	System.String
 
 
-                String sender = myItem.Sender.Address;
+
+                String sender = myItem.SenderEmailAddress;
 
                 HomeworkItem aHomeworkItem = new HomeworkItem
                 {
@@ -51,7 +52,8 @@ namespace SaveAllTheHomework.Source
                 aHomeworkItem.StudentID = GetStudentIDFromWHUEmail(sender);
 
                 // 尝试识别附件文件名凭此获得学号
-                if (aHomeworkItem.StudentID == 0) {
+                if (aHomeworkItem.StudentID == 0)
+                {
                     foreach (dynamic myAttachment in myItem.Attachments)
                     {
                         aHomeworkItem.StudentID = GetStudentIDFromString(myAttachment.FileName);
@@ -60,7 +62,8 @@ namespace SaveAllTheHomework.Source
                 }
 
                 // 尝试识别邮件标题凭此获得学号
-                if (aHomeworkItem.StudentID == 0) {
+                if (aHomeworkItem.StudentID == 0)
+                {
                     aHomeworkItem.StudentID = GetStudentIDFromString(myItem.ConversationTopic);
                 }
 
@@ -69,100 +72,52 @@ namespace SaveAllTheHomework.Source
 
 
                 // 如果都识别不到
-                if (aHomeworkItem.StudentID == 0) {
-                    // 发出警告然后下一个
-                    /*
-                    // Construct the visuals of the toast (using Notifications library)
-                    ToastContent toastContent = new ToastContent()
-                    {
-                        // Arguments when the user taps body of toast
-                        Launch = "action=viewConversation&conversationId=5",
-
-                        Visual = new ToastVisual()
-                        {
-                            BindingGeneric = new ToastBindingGeneric()
-                            {
-                                Children =
-                                {
-                                    new AdaptiveText()
-                                    {
-                                        Text = "Hello world!"
-                                    }
-                                }
-                            }
-                        }
-                    };
-
-                    // Create the XML document (BE SURE TO REFERENCE WINDOWS.DATA.XML.DOM)
-                    var doc = new XmlDocument();
-                    doc.LoadXml(toastContent.GetContent());
-
-                    // And create the toast notification
-                    var toast = new ToastNotification(doc);
-
-                    // And then show it
-                    DesktopNotificationManagerCompat.CreateToastNotifier().Show(toast);
-                    */
-                    MessageBox.Show("" + aHomeworkItem.StudentID);
+                if (aHomeworkItem.StudentID == 0)
+                {
                     continue;
                 };
 
-
-                if (aHomeworkItem.StudentID > 0)
+                // 如果没有附件
+                if (myItem.Attachments.Count == 0)
                 {
-                    MessageBox.Show("" + aHomeworkItem.StudentID);
+                    continue;
                 }
-                else {
-                    MessageBox.Show("" + aHomeworkItem.StudentID);
-                }
+
+                aHomeworkItem.Attachments = myItem.Attachments;
 
                 HomeworkList.Add(aHomeworkItem);
 
-                /*
-                if (myItem.Attachments.Count > 1)
-                {
-
-                    foreach (dynamic myAttachment in myItem.Attachments)
-                    {
-                        myAttachment.SaveAsFile("D:\\AppData\\outlook\\" + myAttachment.FileName);
-                    }
-
-                }
-                else
-                {
-
-                    foreach (dynamic myAttachment in myItem.Attachments)
-                    {
-                        myAttachment.SaveAsFile("D:\\AppData\\outlook\\" + myAttachment.FileName);
-                    }
-
-                }
-                */
 
             }
 
-            MessageBox.Show(ActivedFolder.FullFolderPath+" 里的所有附件都已经保存下来辣！");
-           
+            //HomeworkList.Sort()
+
+
+            MessageBox.Show(ActivedFolder.FullFolderPath + " 里的所有附件都已经保存下来辣！");
+
 
 
 
             return 0;
         }
 
-        private bool IsWhuEMail(String sender) {
+        private bool IsWhuEMail(String sender)
+        {
             String[] Email = sender.Split('@');
             if (!Email[1].ToLower().Equals("whu.edu.cn")) return false;
 
             if (Email[0].Length != 13) return false; // 2018 302 114514 // 4+3+6
 
-            for (int i = 0; i < 13; i++) {
-                if ( (Email[0][i] < '0') || (Email[0][i] > '9') ) return false;
+            for (int i = 0; i < 13; i++)
+            {
+                if ((Email[0][i] < '0') || (Email[0][i] > '9')) return false;
             }
 
             return true;
         }
 
-        private long GetStudentIDFromWHUEmail(String sender) {
+        private long GetStudentIDFromWHUEmail(String sender)
+        {
             String[] Email = sender.Split('@');
             if (!Email[1].ToLower().Equals("whu.edu.cn")) return 0;
             if (Email[0].Length != 13) return 0; // 2018 302 114514 // 4+3+6
