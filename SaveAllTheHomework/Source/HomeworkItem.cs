9d16bab99bd33aa2dd301c82d78ec0ec41efc50b
Eric_Lian <git@SuperExerciseBook.com>
2019-09-28 10:38:50 +0800

    Sort

--- SaveAllTheHomework/Source/HomeworkItem.cs
+++ SaveAllTheHomework/Source/HomeworkItem.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SaveAllTheHomework.Source
 {
-    internal class HomeworkItem
+    internal class HomeworkItem : IComparable, IComparer<HomeworkItem>
     {
         public String Sender = "";
         public long StudentID = 0;
         public dynamic Attachments = null;
-        public DateTime CreationTime = DateTime.UtcNow;
+        public DateTime SentOn = DateTime.UtcNow;
+
+        public int Compare(HomeworkItem x, HomeworkItem y)
+        {
+            if (x.StudentID < y.StudentID) return -1;
+            if (x.StudentID > y.StudentID) return 1;
+            if (x.SentOn > y.SentOn) return 1;
+            if (x.SentOn < y.SentOn) return 1;
+
+            return 0;
+        }
+
+        public int CompareTo(object obj)
+        {
+            return Compare(this, (HomeworkItem)obj);
+        }
+
     }
+
+
 }
